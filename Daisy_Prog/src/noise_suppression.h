ffe183e7206e72dd7282f9bcc8e01de1685ebce4
DanielMadiedo-17 <danielm202111@gmail.com>
2026-04-13 09:32:16 -0400

    feat: add noise suppression module with dr_wav integration

---
+++ Daisy_Prog/src/noise_suppression.h
@@ -0,0 +1,23 @@
+#include <cstddef>
+#include <cstdint>
+
+#define BIN_FRAMES      312
+#define LAMBDA          0.05
+#define ALPHA           20
+#define N               256 //FFT SIZE
+#define OVERSAMP        4   //oversampling factor
+#define NUM_FREQ_BINS   128
+
+typedef struct{
+    float M1[NUM_FREQ_BINS];
+    float M2[NUM_FREQ_BINS];
+    float M3[NUM_FREQ_BINS];
+    float M4[NUM_FREQ_BINS];
+    float mmse[NUM_FREQ_BINS]; //minimum across m1-m4
+    float overlap_buf[N];
+    int count_min;
+}NS_State;
+
+void ns_init(NS_State& state);
+
+void ns_process_block(NS_State& state, const float* input, float* output);
